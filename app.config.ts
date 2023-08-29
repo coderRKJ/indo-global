fc174a7342a4c0780e3ea8ae147b406e937ca1cf
Rahul Kurian Jacob <rahulkurianjacob@gmail.com>
2023-08-29 23:01:31 +0530

    Replace Logo Images

--- app.config.ts
+++ app.config.ts
@@ -11,8 +11,8 @@ export default defineAppConfig({
     header: {
       position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        path: '/images/glob-logo.png', // path of the logo
+        pathDark: '/images/glob-logo-dark.png', // path of the logo in dark mode, leave this empty if you want to use the same logo
         alt: 'Indo Global' // alt of the logo
       }
     },
